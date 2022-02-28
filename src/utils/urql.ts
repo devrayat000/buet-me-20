9a931c42f0558842e99cf38cdddcc49f63241941
devrayat000 <2010050@me.buet.ac.bd>
2022-02-28 14:13:32 -0800

    added env

--- src/utils/urql.ts
+++ src/utils/urql.ts
@@ -3,22 +3,22 @@ import {
     cacheExchange,
     fetchExchange,
     ssrExchange,
-  } from '@urql/core'
-  import { createClient } from 'urql'
-  import type { SSRData } from '@urql/core/dist/types/exchanges/ssr'
-  
-  export const ssrCache = ssrExchange({
+} from '@urql/core'
+import { createClient } from 'urql'
+import type { SSRData } from '@urql/core/dist/types/exchanges/ssr'
+
+export const ssrCache = ssrExchange({
     isClient: process.browser,
     initialState: process.browser ? window.__URQL_DATA__ : undefined,
-  })
-  
-  export const client = createClient({
-    url: 'http://localhost:3001/api/graphql',
+})
+
+export const client = createClient({
+    url: process.env.NEXT_PUBLIC_API_ENDPOINT!,
     exchanges: [dedupExchange, cacheExchange, ssrCache, fetchExchange],
-  })
-  
-  declare global {
+})
+
+declare global {
     interface Window {
-      __URQL_DATA__: SSRData
+        __URQL_DATA__: SSRData
     }
-  }
+}
