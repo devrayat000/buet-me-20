72f399d0f959c7c68d84d657e2b8c12fc6e5f705
devrayat000 <2010050@me.buet.ac.bd>
2022-03-01 03:01:07 +0600

    implemented seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ import Drawer from '../components/common/drawer'
 import DrawerWrapper from '../components/common/drawer-wrapper'
 import { StoreProvider } from 'easy-peasy'
 import store from '../store'
+import Head from 'next/head'
 
 
 
@@ -18,20 +19,27 @@ function MyApp({ Component, pageProps }: MyAppProps) {
   }
 
   return (
-    <StoreProvider store={store} >
-      <DrawerWrapper>
-        <main className='relative drawer-content'>
-          <NavBar />
-          <Provider value={client}>
-            <Component {...(pageProps as any)} />
-          </Provider>
-          <footer className='flex flex-col justify-center items-center fixed bottom-0 inset-x-0 text-sm md:text-base'>
-            <h4>&copy; COPYRIGHT 2022 - BUET&apos;XX</h4>
-            <h4>Developed by <span className='text-red-500'>Zul Ikram musaddik Rayat</span></h4>
-          </footer>
-        </main>
-      </DrawerWrapper>
-    </StoreProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      </Head>
+      <StoreProvider store={store} >
+        <DrawerWrapper>
+          <main className='relative drawer-content'>
+            <NavBar />
+            <Provider value={client}>
+              <Component {...(pageProps as any)} />
+            </Provider>
+            <footer className='flex flex-col justify-center items-center fixed bottom-0 inset-x-0 text-sm md:text-base'>
+              <h4>&copy; COPYRIGHT 2022 - BUET&apos;XX</h4>
+              <h4>Developed by <span className='text-red-500'>Zul Ikram musaddik Rayat</span></h4>
+            </footer>
+          </main>
+        </DrawerWrapper>
+      </StoreProvider>
+    </>
   )
 }
 
--- src/pages/todo.tsx
+++ src/pages/todo.tsx
@@ -3,11 +3,22 @@ import { Tab } from '@headlessui/react'
 import { useState } from "react";
 
 import Notifications from '../components/todo/notifications'
+import { NextSeo } from "next-seo";
 
 const Todo: NextPage = () => {
     const [tab, setTab] = useState(0)
     return (
         <div className="pt-20">
+            <NextSeo
+                title="Todos | Announcements, CTs and Lab Reports"
+                description="Stay up to date with announcements, class tests and lab reports by the class representatives"
+                additionalMetaTags={[
+                    {
+                        name: 'keywords',
+                        content: 'announcement,ct,test,lab'
+                    }
+                ]}
+            />
             <section className="isolate m-4">
                 <Notifications />
             </section>
