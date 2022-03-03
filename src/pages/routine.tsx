5a5464326b6827c73c3e59c2eb1dd3ac578c1377
devrayat000 <2010050@me.buet.ac.bd>
2022-03-03 16:36:56 +0600

    solved routine fetch 3

--- src/pages/routine.tsx
+++ src/pages/routine.tsx
@@ -4,12 +4,13 @@ import type { GetStaticProps, NextPage } from "next";
 import RoutineApp from "../components/routine";
 
 
-const Routine: NextPage<{ routineUrl: string }> = ({ routineUrl }) => {
+const Routine: NextPage<{ routineUrl: string, term: string }> = ({ routineUrl, term }) => {
     const routineRef = useRef<HTMLDivElement>(null)
 
     return (
         <div className="pt-20 mx-16">
             <section ref={routineRef} className="isolate m-4">
+                <h2 className='m-2 text-center'>Routine for {term}</h2>
                 <RoutineApp />
                 <div className="flex justify-end">
                     <a className="btn btn-wide" title="Download Routine" href={routineUrl} download='routine.pdf'>Download</a>
@@ -22,13 +23,19 @@ const Routine: NextPage<{ routineUrl: string }> = ({ routineUrl }) => {
 export default Routine;
 
 export const getStaticProps: GetStaticProps = async (context) => {
-    const res = await fetch('http://localhost:1337/api/upload/files/1',
+    const fileRes = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT!}/api/upload/files/1`,
         { headers: { 'Content-Type': 'application/json' } }
     )
-    const data = await res.json()
+    const fileData = await fileRes.json()
+
+    const routineRes = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT!}/api/routine`,
+        { headers: { 'Content-Type': 'application/json' } }
+    )
+    const routine = await routineRes.json()
     return {
         props: {
-            routineUrl: 'http://localhost:1337' + data.url
+            term: routine.data.attributes.term,
+            routineUrl: process.env.NEXT_PUBLIC_API_ENDPOINT! + fileData.url
         }
     };
 }
